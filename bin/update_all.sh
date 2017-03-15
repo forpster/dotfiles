7c72e80d50dcbef8d8f796491ced549d976d5eeb
Ross Kirk <Ross_Kirk@rapid7.com>
2017-03-15 10:05:52 +0000

    update all repos

---
+++ bin/update_all.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# pull all the things
+cd ~/rapid7
+for dir in `find * -maxdepth 0 -type d -print`;
+do
+    if [ -e $dir/.git ]
+    then
+        (cd $dir && echo -n "$dir " && git branch | grep \* | cut -d ' ' -f2- && echo && git fetch -ap && git pull)
+    fi
+done
+
+# bundle update some of the things
+echo "cukesrc bundle update"
+cd ~/rapid7/nexpose/src/internal/private/cucumber && bundle update
+echo "vuln-tools bundle update"
+cd ~/rapid7/vuln-tools/ && bundle update
